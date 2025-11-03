7f8a247e29f4b385f44a4bcfbaf87f3e810de7f2
yanasnytko-wsc <yana.snytko@wsc.eu>
2025-11-03 14:42:55 +0100

    configs changes

--- evenmont-web/src/services/api.js
+++ evenmont-web/src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const API_BASE = 'http://localhost:8000/api'
+// const API_BASE = 'http://localhost:8000/api'
+const API_BASE =
+  import.meta.env.VITE_API_BASE ||
+  `${import.meta.env.VITE_API_ORIGIN}`
 
 export const api = axios.create({
   baseURL: API_BASE,
